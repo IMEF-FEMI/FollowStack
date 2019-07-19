7111b697c8132dbed0317a5a33347d0bf80e4034
IMEF <bolajifemi28@gmail.com>
2019-07-19 04:38:34 +0100

    g

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const http = require("http");
 const app = require("./server/app");
 
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const server = http.createServer(app);
 const socketIO = require("socket.io");
 
--- server/app.js
+++ server/app.js
@@ -60,13 +60,4 @@ app.use("/api/post", post);
 app.use("/auth", auth);
 // app.use("/api/users-online", usersOnline);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 module.exports = app;
